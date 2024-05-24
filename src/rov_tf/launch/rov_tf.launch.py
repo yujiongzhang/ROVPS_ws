deb6d2cc7624b8c0880145422c012c38270852e5
yujiongzhang <yj_zhang123@foxmail.com>
2024-05-24 09:25:00 +0800

    add rov_tf

---
+++ src/rov_tf/launch/rov_tf.launch.py
@@ -0,0 +1,33 @@
+# 导入库
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    """launch内容描述函数，由ros2 launch 扫描调用"""
+    # sts1000_dc = Node(
+    #     package="msis",
+    #     executable="sts1000_dc"
+    # )
+    # sts1000_process = Node(
+    #     package="msis",
+    #     executable="sts1000_process"
+    # )
+    rov_tf_publisher_01 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0', '0', '0.076', '0', '0', '0', 'base_footprint', 'base_link']
+    )
+    rov_tf_publisher_02 = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0', '0', '0.075.076', '0', '0', '0', 'base_link', 'laser_link']
+    )
+    rov_bringup = Node(
+        package="rov_tf",
+        executable="rov_bringup"
+    )
+    # 创建LaunchDescription对象launch_description,用于描述launch文件
+    launch_description = LaunchDescription(
+        [rov_tf_publisher_01, rov_tf_publisher_02, rov_bringup])
+    # 返回让ROS2根据launch描述执行节点
+    return launch_description
